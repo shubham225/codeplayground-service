19510d72e7ab68536b677d623527ece5117101f4
Shubham Shinde <31509311+shubham225@users.noreply.github.com>
2025-10-24 19:37:36 +0530

    Add validation for functionName in CodeStubDTO

--- src/main/java/com/shubham/codeplayground/model/dto/CodeStubDTO.java
+++ src/main/java/com/shubham/codeplayground/model/dto/CodeStubDTO.java
@@ -3,12 +3,14 @@ package com.shubham.codeplayground.model.dto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
 @Getter
 @Setter
 public class CodeStubDTO {
+    @NotBlank("Function name can't be blank")
     private String functionName;
     private String returnType;
     private boolean returnisArray;
