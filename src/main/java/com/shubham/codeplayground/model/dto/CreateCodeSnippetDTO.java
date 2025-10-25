3b83216a3025ed9ea5973c2410bb39e0b56593fe
Shubham Shinde <31509311+shubham225@users.noreply.github.com>
2025-10-25 17:16:38 +0530

    Add validation for codeSnippet field

--- src/main/java/com/shubham/codeplayground/model/dto/CreateCodeSnippetDTO.java
+++ src/main/java/com/shubham/codeplayground/model/dto/CreateCodeSnippetDTO.java
@@ -1,6 +1,7 @@
 package com.shubham.codeplayground.model.dto;
 
 import com.shubham.codeplayground.model.enums.Language;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 public class CreateCodeSnippetDTO {
     private Language language;
+    @NotBlank
     private String codeSnippet;
     private String mainCode;
     private String answerCode;
