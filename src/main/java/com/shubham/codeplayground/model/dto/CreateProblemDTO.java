2a370b24d4ac9994699f76fb45fd39e54b0a6967
Shubham Shinde <31509311+shubham225@users.noreply.github.com>
2025-10-28 18:39:52 +0530

    Add validation annotations to CreateProblemDTO fields

--- src/main/java/com/shubham/codeplayground/model/dto/CreateProblemDTO.java
+++ src/main/java/com/shubham/codeplayground/model/dto/CreateProblemDTO.java
@@ -1,6 +1,7 @@
 package com.shubham.codeplayground.model.dto;
 
 import com.shubham.codeplayground.model.enums.ProblemDifficulty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.Set;
@@ -11,11 +12,15 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProblemDTO {
+    @NotBlank
     private String type;
+    @NotBlank
     private String title;
+    @NotBlank
     private String descriptionMd;
     private ProblemDifficulty difficulty;
     private String[] languages;
+    @NotBlank
     private CodeStubDTO codeStub;
     private Set<FileDTO> testcases;
     private Set<FileDTO> solutions;
